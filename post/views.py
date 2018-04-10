d987d87dc121794662ea158e1f9e1a20eab401df
Metin Münüklü <mtnmunuklu@gmail.com>
2018-04-10 13:40:47 +0300

    index and Detail View

--- post/views.py
+++ post/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render,HttpResponse
-
+from django.shortcuts import render,HttpResponse, get_object_or_404
+from .models import Post
 # Create your views here.
 
 def post_index(request):
-    return HttpResponse('Burası Post index sayfası')
+    posts = Post.objects.all()
+    return render(request,'post/index.html',{'posts': posts})
 
 def post_detail(request):
-    return HttpResponse('Burası Post detail sayfası')
+    post=get_object_or_404(Post, id=2)
+    context = {
+        'post':post,
+    }
+    return render(request,'post/detail.html', context)
 
 def post_create(request):
     return HttpResponse('Burası Post create sayfası')
