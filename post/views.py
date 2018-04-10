bf16f254cdeaac5f6deb3a436ed900b967c92528
Metin Münüklü <mtnmunuklu@gmail.com>
2018-04-10 19:56:22 +0300

    Updated View and Redirect

--- post/views.py
+++ post/views.py
@@ -1,23 +1,41 @@
-from django.shortcuts import render,HttpResponse, get_object_or_404
+from django.shortcuts import render,HttpResponse, get_object_or_404, HttpResponsePermanentRedirect
 from .models import Post
-# Create your views here.
+from .forms import PostForm
 
 def post_index(request):
     posts = Post.objects.all()
     return render(request,'post/index.html',{'posts': posts})
 
-def post_detail(request):
-    post=get_object_or_404(Post, id=2)
+def post_detail(request,id):
+    post=get_object_or_404(Post, id=id)
     context = {
         'post':post,
     }
     return render(request,'post/detail.html', context)
 
 def post_create(request):
-    return HttpResponse('Burası Post create sayfası')
 
-def post_update(request):
-    return HttpResponse('Burası Post update sayfası')
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        post = form.save()
+        return HttpResponsePermanentRedirect(post.get_absolute_url())
+
+    context = {
+            'form': form
+        }
+
+    return render(request, 'post/form.html', context)
+
+def post_update(request,id):
+    post = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return HttpResponsePermanentRedirect(post.get_absolute_url())
+    context = {
+        'form': form
+    }
+    return render(request, 'post/form.html', context)
 
 def post_delete(request):
     return HttpResponse('Burası Post delete sayfası')
